0ebcc59c998130e86b81e380d27a86709b84e00e
crown.hg <crown.hg@gmail.com>
2022-05-16 11:23:08 +0800

    log-slow-request

--- src/Laravel/Middleware/PlogAfter.php
+++ src/Laravel/Middleware/PlogAfter.php
@@ -12,24 +12,34 @@ class PlogAfter
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
+     * @param int $longtime=3 slow-queries-time, default 3 second
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, int $longtime=3)
     {
         $response =  $next($request);
 
         $data = [
-            'Content'=>$response->getContent(),                    
+            'Content'=>$response->getContent(),
             'headers_list'=>headers_list(),
             'apache_response_headers'=>apache_response_headers(),
             'http_response_code'=>http_response_code(),
             'session'=>session()->all(),
         ];
         // var_dump($data['SEND']);exit;
-        $mesg = sprintf('Middleware,PlogAfter.handle,%s,%s', $_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO']);
+
         $plog = app('dbh')->wlog('db');
+        if(isset($_SERVER['REQUEST_TIME']) && ($longtime >0) ){ // log slow queries, default 3MS
+            $interval = time() - $_SERVER['REQUEST_TIME'];
+            if($interval>$longtime){
+                $mesg = sprintf('Middleware,PlogAfter.handle-slow_%s,%s,%s', $interval, $_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO']);
+                $plog->warning($mesg, $data);
+            }
+        }
+
+        $mesg = sprintf('Middleware,PlogAfter.handle,%s,%s', $_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO']);
         $plog->info($mesg, $data);
-        
+
         return $response;
     }
 }
--- src/thinkphp/middleware/PlogAfter.php
+++ src/thinkphp/middleware/PlogAfter.php
@@ -11,25 +11,35 @@ class PlogAfter
      *
      * @param \think\Request $request
      * @param \Closure       $next
+     * @param int $longtime=3 slow-queries-time, default 3 second
      * @return Response
      */
-    public function handle($request, \Closure $next)
+    public function handle($request, \Closure $next, int $logtime=3)
     {
 
-		$response = $next($request);
+        $response = $next($request);
 
         $data = [
-            'Content'=>$response->getContent(),                    
+            'Content'=>$response->getContent(),
             'headers_list'=>headers_list(),
             'apache_response_headers'=>apache_response_headers(),
             'http_response_code'=>http_response_code(),
-            'session'=>session(''),            
+            'session'=>session(''),
         ];
         // var_dump($data['Content']);exit;
-        $mesg = sprintf('Middleware,PlogAfter.handle,%s,%s', $_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO']);
+
         $plog = app('dbh')->wlog('db');
+        if(isset($_SERVER['REQUEST_TIME']) && ($longtime >0) ){ // log slow queries, default 3MS
+            $interval = time() - $_SERVER['REQUEST_TIME'];
+            if($interval>$longtime){
+                $mesg = sprintf('Middleware,PlogAfter.handle-slow_%s,%s,%s', $interval, $_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO']);
+                $plog->warning($mesg, $data);
+            }
+        }
+
+        $mesg = sprintf('Middleware,PlogAfter.handle,%s,%s', $_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO']);
         $plog->info($mesg, $data);
 
-        return $response;        
+        return $response;
     }
 }
